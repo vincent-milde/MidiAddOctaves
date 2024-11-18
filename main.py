22ac624a0ced69f2ae4cec50acfcafb09c6a6b37
Vincent Milde <vincent.milde@gmx.at>
2024-11-18 13:45:05 +0100

    Added refresh button

--- main.py
+++ main.py
@@ -21,21 +21,33 @@ selected_portIn = tk.StringVar()
 selected_portOut = tk.StringVar()
 
 #Comboboxes
+input_frame = ttk.Frame(master = window)
+output_frame = ttk.Frame(master=window)
 
+#Refresher function
+def refresher():
+    input_combo["values"] = mido.get_input_names()
+    output_combo["values"] = mido.get_output_names()
 # Create a Combobox for selecting the input port
-input_label = ttk.Label(master=window, text="MIDI Input", font='Calibri 12')
+input_label = ttk.Label(master= input_frame, text="MIDI Input", font='Calibri 12')
 input_label.pack(pady=5)
-input_combo = ttk.Combobox(master=window, textvariable=selected_portIn, values=portsIn, state="readonly")
+input_combo = ttk.Combobox(master=input_frame, textvariable=selected_portIn, values=portsIn, state="readonly")
 input_combo.set("Select Input Port")  # Placeholder text
-input_combo.pack(pady=5)
+refresh_input_button = ttk.Button(master = input_frame, text= "Refresh", command=refresher)
+input_combo.pack(side=tk.LEFT, padx=5)
+refresh_input_button.pack(side=tk.LEFT)
+input_frame.pack(pady=5)
 
+    
 # Create a Combobox for selecting the output port
-output_label = ttk.Label(master=window, text="MIDI Output", font='Calibri 12')
+output_label = ttk.Label(master=output_frame, text="MIDI Output", font='Calibri 12')
 output_label.pack(pady=5)
-output_combo = ttk.Combobox(master=window, textvariable=selected_portOut, values=portsOut, state="readonly")
+output_combo = ttk.Combobox(master=output_frame, textvariable=selected_portOut, values=portsOut, state="readonly")
 output_combo.set("Select Output Port")  # Placeholder text
-output_combo.pack(pady=5)
-
+refresh_output_button = ttk.Button(master = output_frame, text= "Refresh", command=refresher)
+output_combo.pack(side=tk.LEFT, padx=5)
+refresh_output_button.pack(side=tk.LEFT)
+output_frame.pack(pady=5)
 # Button to confirm the selection and start the MIDI connection
 def connect_midi():
     input_port_name = selected_portIn.get()
@@ -47,7 +59,6 @@ def connect_midi():
 
 def disconnect_midi():
     midi_handler.close_ports()  # Close the ports and stop listening
-
 # Connect and Disconnect buttons
 connect_button = ttk.Button(window, text="Connect", command=connect_midi)
 connect_button.pack(pady=10)
